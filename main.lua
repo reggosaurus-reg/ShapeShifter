b69ee5b82a2855c9642d8cd26d28fc675f012285
Gustav Sörnäs <gusso230@student.liu.se>
2019-11-09 12:38:11 +0100

    Refactor player

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+require("player")
+
 -- KEYMAP
 stretch_pos = "j"
 stretch_neg = "k"
@@ -15,60 +17,28 @@ rotation_speed = math.pi * 2  -- half a lap per second
 
 function love.load()
 	love.window.setMode(win_w, win_h, {resizable=false, vsync=true, highdpi=true})
-	player = {}
-	player.diameter = 200
-	player.height = player.diameter / 2
-	player.width = player.diameter / 2
-	player.x = 0
-	player.y = 0
-	player.rot = 0  -- radians
-	player.round = 0;  -- andel, 0 = kvadrat, 1 = cirkel/ellips
-
-end
-
-function calcPlayer()
-	player.x = (win_h - player.width) / 2
-	player.y = (win_w - player.height) / 2
 end
 
 function love.keypressed(key)
-	if key == "space" then
-		print(player.width)
-		print(player.width / 2)
-		print(player.height)
-		print(player.height / 2)
-		print(player.rot)
+	if key == rot_r then
+		player.rotation_dir = player.rotation_dir + 1
+	elseif key == rot_l then
+		player.rotation_dir = player.rotation_dir - 1
+	end
+end
+
+function love.keyreleased(key)
+	if key == rot_r then
+		player.rotation_dir = player.rotation_dir - 1
+	elseif key == rot_l then
+		player.rotation_dir = player.rotation_dir + 1
 	end
 end
 
 function love.update(dt)
-	-- PLAYER 
-	-- rectangle/square
-	if love.keyboard.isDown(stretch_pos) then
-		player.width = player.width + stretch_speed * dt
-	elseif love.keyboard.isDown(stretch_neg) then
-		player.width = player.width - stretch_speed * dt
-	end
-	player.width = math.min(math.max(player.diameter / 10, player.width), player.diameter * (9 / 10))
-	player.height = player.diameter - player.width
-	
-	-- roundness
-	if love.keyboard.isDown(round_inc) then
-		player.round = player.round + round_speed * dt
-	elseif love.keyboard.isDown(round_dec) then
-		player.round = player.round - round_speed * dt
-	end
-	player.round = math.min(math.max(0, player.round), 1)
+	player.rotation = player.rotation + player.rotation_dir * rotation_speed * dt
 
-	-- rotation
-	if love.keyboard.isDown(rot_r) then
-		player.rot = player.rot + rotation_speed * dt
-	elseif love.keyboard.isDown(rot_l) then
-		player.rot = player.rot - rotation_speed * dt
-	end
-	calcPlayer()
-
-	-- SHOTS
+	player.update()
 end
 
 draw_player = true
@@ -76,15 +46,7 @@ draw_shots = false
 
 function love.draw()
 	if draw_player == true then
-		love.graphics.push()
-			love.graphics.translate(win_w / 2, win_h / 2)
-			love.graphics.rotate(player.rot)
-			love.graphics.rectangle("line", - (player.width / 2), - (player.height / 2), 
-					player.width, player.height,
-					(player.width / 2) * player.round,
-					(player.height / 2) * player.round
-			)
-		love.graphics.pop()
+		player.draw()
 	end
 
 	if draw_shots == true then
