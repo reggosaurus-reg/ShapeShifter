4b196616f39e9a6717e5c548d25efac33526b0cf
Regina Hansson <regha434@student.liu.se>
2019-11-09 19:39:32 +0100

    Indent modes.lua

--- modes.lua
+++ modes.lua
@@ -1,52 +1,53 @@
 require("objects")
 
-mode_see = {func_draw = function() 
-				love.graphics.print("SEE", 0, 0)
-				draw_ellipse(player)
-				draw_shot()
-				draw_enemies()
-			  end,
-		  func_update = function(dt) 
-			  		--move(player,dt) 
-		  			rotate(player, dt)
-					update(player)
-					update_other_objects(dt)
-				end,
-		  func_shoot = function()
-			  end
-			}
+mode_see = {
+	func_draw = function() 
+		love.graphics.print("SEE", 0, 0)
+		draw_ellipse(player)
+		draw_shot()
+		draw_enemies()
+	end,
+	func_update = function(dt) 
+		rotate(player, dt)
+		update(player)
+		update_other_objects(dt)
+	end,
+	func_shoot = function()
+	end
+}
 
-mode_move = {func_draw = function() 
-				 love.graphics.print("MOVE", 0, 0)
-				 draw_rectangle(player) 
-			   end,
-			  func_update = function(dt) 
-				  		move(player,dt) 
-						rotate(player, dt)
-						update(player)
-						update_other_objects(dt)
-					end,
-			  func_shoot = function()
-			  end
-		   }
+mode_move = {
+	func_draw = function() 
+		love.graphics.print("MOVE", 0, 0)
+		draw_rectangle(player) 
+	end,
+	func_update = function(dt) 
+		move(player,dt) 
+		rotate(player, dt)
+		update(player)
+		update_other_objects(dt)
+	end,
+	func_shoot = function()
+	end
+}
 
-mode_attack = {func_draw = function() 
-				   love.graphics.print("ATTACK", 0, 0)
-				   draw_triangle(player) 
-				   draw_shot()
-				   draw_enemies()
-			     end,
-			  func_update = function(dt) 
-				  		--move(player,dt) 
-						rotate(player, dt)
-						update(player)
-						update_other_objects(dt)
-					end,
-			  func_shoot = function()
-				  		spawn_shot()
-				  		spawn_enemy() -- TODO: Move into some interval function
-					end
-			 }
+mode_attack = {
+	func_draw = function() 
+		love.graphics.print("ATTACK", 0, 0)
+		draw_triangle(player) 
+		draw_shot()
+		draw_enemies()
+	end,
+	func_update = function(dt) 
+		rotate(player, dt)
+		update(player)
+		update_other_objects(dt)
+	end,
+	func_shoot = function()
+		spawn_shot()
+		spawn_enemy() -- TODO: Move into some interval function
+	end
+}
 
 function draw_shot()
 	for i, shot in pairs(shots) do
